ce90e94b1e320b55e871b830c3ff57cc69ee85ea
Jochen Schalanda <jochen@schalanda.name>
2019-11-17 17:54:10 +0100

    Make sure that timeline is being generated

--- bin/update-reports.sh
+++ bin/update-reports.sh
@@ -6,6 +6,7 @@ mkdir -p "${BUILD_DIR}/json"
 
 for profile in profiles/*.json
 do
-	japi-tracker -build -global-index -rss -json-report "${BUILD_DIR}/json" -deploy "${BUILD_DIR}" "${profile}"
+	japi-tracker -build -global-index -rss -deploy "${BUILD_DIR}" "${profile}"
+	japi-tracker -json-report "${BUILD_DIR}/json" -deploy "${BUILD_DIR}" "${profile}"
 done
 mv index.html "${BUILD_DIR}"
