c686447e53859e4ab06cf4f0a9ecc329a0cad58e
Jochen Schalanda <jochen@schalanda.name>
2022-04-14 20:01:06 +0200

    Fix install script

--- bin/install.sh
+++ bin/install.sh
@@ -6,10 +6,10 @@ INSTALLER_VERSION='0.5'
 
 apt-get update --quiet
 apt-get --no-install-recommends --quiet --assume-yes install \
-	curl make file gawk binutils diffutils rfcdiff wdiff perl gnuplot
+	curl ca-certificates make file gawk binutils diffutils rfcdiff wdiff perl gnuplot
 
 mkdir "${INSTALLER_DIR}"
-curl -s -f -L "https://github.com/lvc/installer-4j/archive/${INSTALLER_VERSION}.tar.gz" | tar --strip-components 1 -C "${INSTALLER_DIR}" -xzvf -
+curl -s -f -L "https://github.com/lvc/installer-4j/archive/refs/tags/${INSTALLER_VERSION}.tar.gz" | tar --strip-components 1 -C "${INSTALLER_DIR}" -xzvf -
 
 pushd "${INSTALLER_DIR}"
 for dep in japi-tracker japi-monitor japi-compliance-checker pkgdiff
